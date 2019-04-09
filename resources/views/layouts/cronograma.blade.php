70dc34f534224c83b9d50c308a01965ccb2b2502
Gabriel Scaranello <gabrieljscaranello@gmail.con>
2019-04-09 01:06:14 -0300

    layout ajust

--- resources/views/layouts/cronograma.blade.php
+++ resources/views/layouts/cronograma.blade.php
@@ -52,64 +52,62 @@
 
 </div> --}}
 
-<div class="container">
-    <div class="col-12 rounded">
-        <div class="row">
-            <div class="col-12">
-                @foreach ($cronograms as $element)
-                <div class="col-12 mb-3 day">
-                    <div class="row text-light bg-dark py-2 font-weight-bold">
-                        <div class="col-12 col-md-6 text-left">
-                            {{$element[0]->dia_mes}} - {{$element[0]->dia_semana}}
-                        </div>
-                        <div class="col-12 col-md-6 text-right d-none d-md-block">Cronograma TADS</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12 px-1">
+            @foreach ($cronograms as $element)
+            <div class="col-12 mb-3 day">
+                <div class="row text-light bg-dark py-2 font-weight-bold">
+                    <div class="col-12 col-md-6 text-left">
+                        {{$element[0]->dia_mes}} - {{$element[0]->dia_semana}}
                     </div>
-                    @foreach ($element as $value)
-                    @if ($value->type != 'mini-curso')
-                    <div class="row hour">
-                        <div class="py-2 col-12 col-md-3 col-lg-2 text-left bg-light text-dark font-weight-bold d-flex align-items-center">
-                            {{substr($value->horario_inicio, 0,5)}} - {{substr($value->horario_fim, 0, 5)}}
-                        </div>
-                        <div class="item py-2 col-12 col-md-9 col-lg-10 text-left text-light" style="background-color: {{$value->cor}}">
-                            <p class="text-center">
-                                {{$value->title}}
-                            </p>
-                        </div>
+                    <div class="col-12 col-md-6 text-right d-none d-md-block">Cronograma TADS</div>
+                </div>
+                @foreach ($element as $value)
+                @if ($value->type != 'mini-curso')
+                <div class="row hour">
+                    <div class="py-2 col-12 col-md-3 col-lg-2 text-left bg-light text-dark font-weight-bold d-flex align-items-center">
+                        {{substr($value->horario_inicio, 0,5)}} - {{substr($value->horario_fim, 0, 5)}}
                     </div>
-                    @endif
-                    @endforeach
+                    <div class="item py-2 col-12 col-md-9 col-lg-10 text-left text-light" style="background-color: {{$value->cor}}">
+                        <p class="text-center">
+                            {{$value->title}}
+                        </p>
+                    </div>
+                </div>
+                @endif
+                @endforeach
 
 
-                    <div class="row hour">
-                        <div class="py-2 col-12 col-md-3 col-lg-2 font-weight-bold text-dark d-flex align-items-center">
-                            @for ($i = 0; $i
-                            < sizeof($element); $i++) @if ($element[$i]->type == 'mini-curso')
-                            {{substr($element[$i]->horario_inicio, 0, 5)}} - {{substr($element[$i]->horario_fim, 0, 5)}}
-                            @break
-                            @endif
-                            @endfor
-                        </div>
-                        <div class="col-12 col-md-9 col-lg-10">
-                            <div class="col-12 px-0">
-                                <div class="row">
-                                    @foreach ($element as $value)
-                                    @if ($value->type == 'mini-curso')
-                                    <div class="item item-secondary py-2 col-12 col-lg-6 text-left text-light" style="background-color: {{$value->cor}}">
-                                        <p class="text-center">
-                                            {{$value->title}}
-                                        </p>
-                                    </div>
-                                    @endif
-                                    @endforeach
+                <div class="row hour">
+                    <div class="py-2 col-12 col-md-3 col-lg-2 font-weight-bold text-dark d-flex align-items-center">
+                        @for ($i = 0; $i
+                        < sizeof($element); $i++) @if ($element[$i]->type == 'mini-curso')
+                        {{substr($element[$i]->horario_inicio, 0, 5)}} - {{substr($element[$i]->horario_fim, 0, 5)}}
+                        @break
+                        @endif
+                        @endfor
+                    </div>
+                    <div class="col-12 col-md-9 col-lg-10">
+                        <div class="col-12 px-0">
+                            <div class="row">
+                                @foreach ($element as $value)
+                                @if ($value->type == 'mini-curso')
+                                <div class="item item-secondary py-2 col-12 col-lg-6 text-left text-light" style="background-color: {{$value->cor}}">
+                                    <p class="text-center">
+                                        {{$value->title}}
+                                    </p>
                                 </div>
+                                @endif
+                                @endforeach
                             </div>
                         </div>
                     </div>
                 </div>
-
-
-                @endforeach
             </div>
+
+
+            @endforeach
         </div>
     </div>
 </div>
