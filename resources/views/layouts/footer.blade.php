5eaf09af3c811255424bb1355117cf9c5fb73fff
Emanuel Schulz <emanuelschulz1999@gmail.com>
2019-04-18 20:14:59 -0300

    Footer top

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,48 +4,61 @@
     <div class="footer_bottom_area">
         <div class="container">
             <div class="row ">
-              
-                {{-- <div class="col-12 col-md-4">
+                <div class="col-12 col-md-3">
                     <div class="footer_bottom">
                         <h6 class="footer-title">Saiba Mais Sobre o Projeto</h6>
                         <div>
-                            <p class="footer-paragraph"></p>
+                            <p class="footer-paragraph">
+                                A Semana de cursos do Eixo de Informação e Comunicação do IFPR - Campus Assis Chateaubriand, este ano está sendo organizada pelos alunos do curso de TADS.
+                            </p>
                         </div>
                     </div>
                 </div>
-
-                <div class="col-12 col-md-4">
+                <div class="col-12 col-md-6">
                     <div class="footer_bottom">
                         <h6 class="footer-title">Conheça a equipe da Semana de Cursos</h6>
-                        <div class="text-left">
-                            <p class="teams"><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Equipe de Palestras</a></p>
-                            <p class="teams"><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Equipe de Minicursos</a></p>
-                            <p class="teams"><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Equipe de Fórum</a></p>
-                            <p class="teams"><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Equipe de Recepção</a></p>
-                            <p class="teams"><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Equipe de Credenciamento</a></p>
-                            <p class="teams"><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Equipe de Coffee Break</a></p>
-                            <p class="teams"><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Equipe de Certificação</a></p>
-                            <p class="teams"><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Equipe de Saúde e Segurança</a></p>
-                            <p class="teams"><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Equipe de Inscrições</a></p>
-                            <p class="teams"><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Equipe de Divulgação</a></p>
-                            <p class="teams"><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#">Equipe de **********</a></p>
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="text-left">
+                                    <p class="teams"><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#"> Equipe de Cerimonial</a></p>
+                                    <p class="teams"><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#"> Equipe de Certificação</a></p>
+                                    <p class="teams"><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#"> Equipe de Coffee Break</a></p>
+                                    <p class="teams"><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#"> Equipe de Credenciamento</a></p>
+                                    <p class="teams"><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#"> Equipe de Divulgação</a></p>
+                                    <p class="teams"><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#"> Equipe de Fórum</a></p>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="text-left">
+                                    <p class="teams"><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#"> Equipe de Inscrições</a></p>
+                                    <p class="teams"><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#"> Equipe de Minicursos</a></p>
+                                    <p class="teams"><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#"> Equipe de Palestras</a></p>
+                                    <p class="teams"><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#"> Equipe de Recepção</a></p>
+                                    <p class="teams"><i class="fa fa-angle-right" aria-hidden="true"></i><a href="#"> Equipe de Saúde e Segurança</a></p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-
-                <div class="col-12 col-md-4">
+                <div class="col-12 col-md-3">
                     <div class="footer_bottom">
                         <h6 class="footer-title">Apoiadores da Semana de Cursos</h6>
                     </div>
-                </div> --}}
-                <div class="col-12">
-                    <div class="footer_bottom">
-                        <p>Desenvolvido por <a href="https://www.facebook.com/dijaygabriel" target="_blank">Gabriel Scaranello</a> e <a target="_blank" href="https://www.facebook.com/mano.schulz.5">Emanuel Schulz</a></p>
-                    </div>
                 </div>
-
             </div>
+
+
+
+
+
+            <div class="col-12 p-0 m-0">
+                <div class="footer_bottom">
+                    <p>Desenvolvido por <a href="https://www.facebook.com/gabrieljscaranello" target="_blank">Gabriel Scaranello</a> e <a target="_blank" href="https://www.facebook.com/emanuelhschulz">Emanuel Schulz</a></p>
+                </div>
+            </div>
+
         </div>
+    </div>
     </div>
 </footer>
 <!-- ***** Footer Area End ***** -->
